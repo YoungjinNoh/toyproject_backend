b1fe138bd52c1912c8338e5c37fe23c073bddd1b
YoungjinNoh <youngjin_noh@naver.com>
2022-08-26 11:53:43 +0900

    Initial commit

---
+++ src/main/java/com/youngjin/toyproject_backend/ToyprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youngjin.toyproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectBackendApplication.class, args);
+	}
+
+}
